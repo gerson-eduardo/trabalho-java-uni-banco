fd11711774fe74a35d1fd1ddf864e8ad2ccdaabd
eduardo <gersonabes@gmail.com>
2023-09-28 19:25:49 -0300

    feat: criei a estrutura base do projeto

---
+++ conta-bancaria/src/main/java/com/bytebankers/contabancaria/ContaBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.bytebankers.contabancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBancariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContaBancariaApplication.class, args);
+    }
+
+}
---
+++ conta-bancaria/src/test/java/com/bytebankers/contabancaria/ContaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebankers.contabancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBancariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
