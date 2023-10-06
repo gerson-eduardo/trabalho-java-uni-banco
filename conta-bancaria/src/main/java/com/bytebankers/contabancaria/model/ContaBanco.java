2ec9d50b7c0cb0c3a8e99aa07d7d331c22335144
eduardo <gersonabes@gmail.com>
2023-10-06 17:09:21 -0300

    refact: movi a descrição do objeto para a classe toString e alterei os prints de algumas funções

--- conta-bancaria/src/main/java/com/bytebankers/contabancaria/model/ContaBanco.java
+++ conta-bancaria/src/main/java/com/bytebankers/contabancaria/model/ContaBanco.java
@@ -39,18 +39,11 @@ public class ContaBanco {
     return this.status;
    }
    public void setStatus(boolean Novostatus){
-    this.status = Novostatus;https://github.com/Eduard0-GE/trabalho-java-uni-banco/pull/3/conflict?name=conta-bancaria%252Fsrc%252Fmain%252Fjava%252Fcom%252Fbytebankers%252Fcontabancaria%252Fmodel%252FContaBanco.java&ancestor_oid=a94d074612f053c67553299b02fbe01c92de4c27&base_oid=b97f58a32c1f7a0a3e3cffec50c6271fb495a0bc&head_oid=a062253dbbdc5dc8fe20b360115e74037455c00f
+    this.status = Novostatus;
    }
-   void status ()
-        {
-        System.out.println("Número da conta: " + this.numconta);
-        System.out.println("Tipo de conta:" + this.tipo);
-        System.out.println("Nome do titular da conta: " + this.dono);
-        System.out.println("Saldo da conta: " + this.saldo);
-        System.out.println("A conta está aberta? " + this.status);
-    }
+
     public void abrirConta() {
-        if(this.status == false){
+        if(!this.status){
             System.out.println("Abrindo conta!");
             this.status = true;
             if(this.tipo == "CC"){
@@ -60,16 +53,25 @@ public class ContaBanco {
                  this.saldo = 150.f;
             }
         }else{
-            System.out.println("Sua conta já está aberta! ");
+            System.out.println("Sua conta já está aberta!");
         }
     } 
     
     public void depositar(float deposito){
-        if(this.status == false){
-            System.out.println("Voce nao pode depositar dinheiro, ative sua conta!");
+        if(!this.status){
+            System.out.println("Ative a sua conta para poder fazer um depósito dinheiro");
         }else{
             this.saldo += deposito;
-            System.out.println("");   
+            System.out.println("Dinheiro depositado: "+ this.saldo);
         } 
+    }
+
+    @Override
+    public String toString() {
+        return  "Número da conta: " + this.numconta + "\n" +
+                "Tipo de conta: " + this.tipo + "\n" +
+                "Nome do titular da conta: " + this.dono + "\n" +
+                "Saldo da conta: " + this.saldo + "\n" +
+                "A conta está aberta? " + this.status;
     }
 }
